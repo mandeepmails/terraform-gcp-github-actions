756c0536535a0afdfda4e8d4734a54a06a5cb5ee
mandeepmails <mk@mba.local>
2024-06-21 03:03:47 +0530

    from gcs to gce

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+/*
 resource "google_storage_bucket" "raw" {
   project = var.project
   name = "${var.data-project}-raw2"
@@ -6,4 +7,52 @@ resource "google_storage_bucket" "raw" {
   location = var.region
   labels = local.labels
 
+}
+*/
+
+resource "google_service_account" "default" {
+  account_id   = "userthree@learningsokay.site"
+  display_name = "userthree"
+}
+
+resource "google_compute_instance" "myvm1" {
+  name         = "jhadi-instance-01"
+  machine_type = "n2-standard-1"
+  zone         = "us-central1-c"
+
+  tags = ["finance", "dept"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "accounting"
+      }
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "NVME"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    dept = "finance"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = userthree
+    scopes = ["cloud-platform"]
+  }
 }
